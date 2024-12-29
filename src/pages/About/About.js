83f8038139fcbcdea737be89bb65ed842a3caacc
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2024-12-28 21:28:31 -0500

    added about and contact pages

---
+++ src/pages/About/About.js
@@ -0,0 +1,26 @@
+export const About = () => {
+  return (
+    <section className='Container'>
+      <div className='About'>
+        <h1 className='AboutHeader'>About</h1>
+        <p className='AboutText'>
+          I’ve been lucky enough to design across a variety of mediums, starting
+          in print and making my way into digital early on—working on iPad and
+          iPhone interfaces before they became the norm. After a software
+          development bootcamp, I spent a few years coding and freelancing, but
+          design has always been my true passion. Outside of work, I’m all about
+          making—quilting, silversmithing, and experimenting in the kitchen.
+          These creative outlets keep me inspired and shape my work in UX/UI.
+          I’ve lived in New York for decades, and the city (plus my hiking trips
+          around it) constantly sparks new ideas. Travel also feeds my
+          creativity—recent trips to Croatia and an upcoming adventure in
+          Merida, Mexico, are already inspiring new thoughts. Today, I bring
+          together my design and tech experience to create digital experiences
+          that are not only visually engaging but also intuitive and
+          user-focused. Whether it's a website, app, or prototype, my goal is
+          always to craft seamless, enjoyable user journeys.
+        </p>
+      </div>
+    </section>
+  )
+}
