842c1ecda45f03641d9f066dee061b771842822e
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-01-04 15:30:08 -0500

    ideate section done

--- src/pages/CaseStudies/Ssc.js
+++ src/pages/CaseStudies/Ssc.js
@@ -2,6 +2,7 @@ import './CaseStudies.css'
 import { Opener } from '../../components/Ux/ReusableComponents/Opener'
 import { Research } from '../../components/Ux/ReusableComponents/Research'
 import { Define } from '../../components/Ux/ReusableComponents/Define'
+import { Ideate } from '../../components/Ux/ReusableComponents/Ideate'
 
 export const Ssc = () => {
   return (
@@ -9,6 +10,7 @@ export const Ssc = () => {
       <Opener />
       <Research />
       <Define />
+      <Ideate />
     </section>
   )
 }
