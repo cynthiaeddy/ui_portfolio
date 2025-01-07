006554e19824bfed0e8d8c7156a70200d7feeb68
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-01-07 16:42:50 -0500

    added define

--- src/pages/CaseStudies/Nyt.js
+++ src/pages/CaseStudies/Nyt.js
@@ -1,24 +1,19 @@
 import './CaseStudies.css'
 import { OpenerNyt } from '../../components/Ux/ReusableComponents/Nyt/OpenerNyt'
 import { ResearchNyt } from '../../components/Ux/ReusableComponents/Nyt/ResearchNyt'
-// import { Define } from '../../components/Ux/ReusableComponents/Define'
-// import { Ideate } from '../../components/Ux/ReusableComponents/Ideate'
-// import { Design } from '../../components/Ux/ReusableComponents/Design'
-// import { Testing } from '../../components/Ux/ReusableComponents/Testing'
-// import { Prototype } from '../../components/Ux/ReusableComponents/Prototype'
-// import { Reflection } from '../../components/Ux/ReusableComponents/Reflection'
+import { DefineNyt } from '../../components/Ux/ReusableComponents/Nyt/DefineNyt'
 
 export const Nyt = () => {
   return (
     <section className='Container studies'>
       <OpenerNyt />
       <ResearchNyt />
-      {/* <Define />
-      <Ideate />
+      <DefineNyt />
+      {/* <Ideate />
       <Design />
       <Testing />
       <Prototype />
-      <Reflection /> */}
+      <Reflection />  */}
     </section>
   )
 }
