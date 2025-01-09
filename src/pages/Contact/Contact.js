3186444e3b4cd4ebc8ea2340e5a01ddf523f978f
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-01-08 21:44:17 -0500

    updated css contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -62,7 +62,7 @@ export const Contact = () => {
       <div className='Container_overlord'>
         {isSubmitted ? (
           // UI shown after successful submission
-          <section className='SecondContainer sucess'>
+          <section className='SecondContainer success'>
             <div className='AboutContact'>thank you!</div>
             <h5 className='h5 contact'>
               I&apos;ll be sure to get back to you within 48 hours.
