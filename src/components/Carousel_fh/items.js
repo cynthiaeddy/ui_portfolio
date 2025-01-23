493bef7be6c9f3a6cf9def912bc7e104d634af6d
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-01-22 22:08:49 -0500

    updated carousel correct order

--- src/components/Carousel_fh/items.js
+++ src/components/Carousel_fh/items.js
@@ -1,5 +1,5 @@
-import item1 from '../../assets/fh/carousel/fh_car_desktop1.png'
-import item2 from '../../assets/fh/carousel/fh_car_desktop2.png'
+import item2 from '../../assets/fh/carousel/fh_car_desktop1.png'
+import item1 from '../../assets/fh/carousel/fh_car_desktop2.png'
 import item3 from '../../assets/fh/carousel/fh_car_desktop3.png'
 import item4 from '../../assets/fh/carousel/fh_car_desktop4.png'
 export const items = [
