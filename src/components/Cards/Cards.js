2bc426e7dbecbc1b69e46db2a72bf7c376e404fc
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-04-05 15:18:25 -0400

    cards updated with visuals and context

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,13 +1,35 @@
 import { Link } from 'react-router-dom'
 import james_desktop from '../../assets/opener_cards/james_combine.png'
+import ssc from '../../assets/opener_cards/soaptriad.png'
+import wh from '../../assets/opener_cards/whcombine.png'
+import nyt from '../../assets/opener_cards/nyt_triad.png'
+import './Cards.css'
+import fhd from '../../assets/opener_cards/fh_combine.png'
+import './Cards.css'
+import './Cards.css'
+import singles from '../../assets/opener_cards/singles_5.png'
 import './Cards.css'
 export const Cards = () => {
   return (
     <>
       <section className='CardContainerHome'>
         <Link to='' className='IndieCard'>
+          <img className='CardImageHome ssc' src={ssc} alt={ssc} />
+          <div className='CardDek'>
+            <h2 className='Card_hed'>Sustainable Soap Co.</h2>
+            <div className='hed_and_blurb'>
+              <h6 className='Card_blurb'>mobile ux/ui design</h6>
+              <p className='Card_p'>
+                End to end redesign of a e-commerce website refining user
+                experience and brand identity.
+              </p>
+            </div>
+            <button className='Button_website'>view</button>
+          </div>
+        </Link>
+        <Link to='' className='IndieCard'>
           <img
-            className='CardImageHome jamesdt'
+            className='CardImageHome'
             src={james_desktop}
             alt={james_desktop}
           />
@@ -15,18 +37,74 @@ export const Cards = () => {
             <h2 className='Card_hed'>World Food</h2>
             <div className='hed_and_blurb'>
               <h6 className='Card_blurb'>
-                responsive UI and software development
+                responsive UI design/software development
               </h6>
               <p className='Card_p'>
-                End to end live project designed and built for James
-                Oseland&apos;s World Food Book Series
+                End to end project designed and built for James Oseland&apos;s
+                World Food book series.
               </p>
             </div>
             <button className='Button_website'>view</button>
           </div>
         </Link>
         <Link to='' className='IndieCard'>
-          <img className='CardImage' src={james_desktop} alt={james_desktop} />
+          <img className='CardImageHome wh' src={wh} alt={wh} />
+          <div className='CardDek'>
+            <h2 className='Card_hed'>Women&apos;s Health</h2>
+            <div className='hed_and_blurb'>
+              <h6 className='Card_blurb'>mobile and tablet ui design</h6>
+              <p className='Card_p'>
+                UI design lead for digital publications, optimizing engagement
+                through user-centered layouts and dynamic digital content.
+              </p>
+            </div>
+            <button className='Button_website'>view</button>
+          </div>
+        </Link>
+        <Link to='' className='IndieCard'>
+          <img className='CardImageHome nyt' src={nyt} alt={nyt} />
+          <div className='CardDek'>
+            <h2 className='Card_hed'>New York Times Cooking App</h2>
+            <div className='hed_and_blurb'>
+              <h6 className='Card_blurb'>mobile Ux/Ui design</h6>
+              <p className='Card_p'>
+                Add a feature project which enables users to adjust a
+                recipe&apos;s serving size.
+              </p>
+            </div>
+            <button className='Button_website'>view</button>
+          </div>
+        </Link>
+        <Link to='' className='IndieCard'>
+          <img className='CardImageHome jamesdt' src={fhd} alt={fhd} />
+          <div className='CardDek'>
+            <h2 className='Card_hed'>Feline Health</h2>
+            <div className='hed_and_blurb'>
+              <h6 className='Card_blurb'>responsive ux/ui design</h6>
+              <p className='Card_p'>
+                Responsive redesign of a veterinary clinic website, enhancing
+                experience and functionality.
+              </p>
+            </div>
+            <button className='Button_website'>view</button>
+          </div>
+        </Link>
+        <Link to='' className='IndieCard'>
+          <img className='CardImageHome jamesdt' src={singles} alt={singles} />
+          <div className='CardDek'>
+            <h2 className='Card_hed'>Editorial Design</h2>
+            <div className='hed_and_blurb'>
+              <h6 className='Card_blurb'>
+                Layout, typography, storytelling
+                {/* Layout, typography, and visual storytelling */}
+              </h6>
+              <p className='Card_p'>
+                Editorial design for multiple print publications, blending
+                content, structure, and visual flow.
+              </p>
+            </div>
+            <button className='Button_website'>view</button>
+          </div>
         </Link>
       </section>
     </>
--- src/components/Fh/PrototypeFh.js
+++ src/components/Fh/PrototypeFh.js
@@ -8,7 +8,7 @@ export const PrototypeFh = () => {
       <div className='prototype_container_desktop'>
         <img className='prototype fh_desktop' src={opener} />
         <Link
-          to='https://www.figma.com/proto/a6XJCrlXeIL4GX3rwHzDtU/feline-health?page-id=904%3A13501&node-id=1365-2532&node-type=frame&viewport=444%2C324%2C0.25&t=p055KIT4rfUmUI41-1&scaling=scale-down&content-scaling=fixed'
+          to='https://www.figma.com/proto/MHrSzsnZHUUvWpdgJT16nM/feline-health-desktop?page-id=19%3A1025&node-id=19-1026&viewport=164%2C112%2C0.5&t=ggCjAhC3Tu8LI57Y-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=19%3A1026'
           target='_blank'
           rel='noopener noreferrer'
           className='ButtonContainer'
@@ -19,7 +19,7 @@ export const PrototypeFh = () => {
       <div className='prototype_container_mobile'>
         <img className='prototype fh_mobile' src={openerMobile} />
         <Link
-          to='https://www.figma.com/proto/a6XJCrlXeIL4GX3rwHzDtU/feline-health?page-id=1147%3A2545&node-id=1147-2720&node-type=frame&viewport=-31%2C2297%2C0.5&t=8CUFSB64dyvT3lRT-1&scaling=scale-down&content-scaling=fixed'
+          to='https://www.figma.com/proto/a6XJCrlXeIL4GX3rwHzDtU/feline-health?page-id=1147%3A2545&node-id=1147-2563&viewport=35%2C1106%2C0.19&t=hkshLzhBQx9TTpcM-1&scaling=scale-down&content-scaling=fixed'
           target='_blank'
           rel='noopener noreferrer'
           className='ButtonContainer'
