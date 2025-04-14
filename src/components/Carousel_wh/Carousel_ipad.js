32ae5cfb57c5e692b9f20667f19721803eb5d1fb
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-04-14 15:39:04 -0400

    adjusted slide dots

--- src/components/Carousel_wh/Carousel_ipad.js
+++ src/components/Carousel_wh/Carousel_ipad.js
@@ -7,36 +7,14 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Carousel_slide_ipad from './Carousel_slide_ipad'
 
-const NextArrow = ({ onClick, style }) => (
-  <div
-    className='custom-arrow next-arrow'
-    onClick={onClick}
-    style={{
-      ...style,
-      position: 'absolute',
-      top: '50%',
-      right: '-100px',
-      transform: 'translateY(-50%)',
-      zIndex: 2,
-    }}
-  >
+const NextArrow = ({ onClick }) => (
+  <div className='custom-arrow next-arrow ipad' onClick={onClick}>
     <FaArrowRight color='grey' size={14} />
   </div>
 )
 
-const PrevArrow = ({ onClick, style }) => (
-  <div
-    className='custom-arrow prev-arrow ipad'
-    onClick={onClick}
-    style={{
-      ...style,
-      position: 'absolute',
-      top: '50%',
-      left: '-100px',
-      transform: 'translateY(-50%)',
-      zIndex: 2,
-    }}
-  >
+const PrevArrow = ({ onClick }) => (
+  <div className='custom-arrow prev-arrow ipad' onClick={onClick}>
     <FaArrowLeft color='grey' size={14} />
   </div>
 )
