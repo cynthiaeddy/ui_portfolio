c1eee536e90fbe6909d767e952ea87c6377ddd02
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2025-10-01 11:10:07 -0400

    update opener mobile

--- src/components/Ssc/OpenerMobile.js
+++ src/components/Ssc/OpenerMobile.js
@@ -1,15 +1,16 @@
 import '../ReusableComponents.css'
-import soap_opener from '../../assets/ssc/soap_opener_sm.png'
-import soap_ive from '../../assets/ssc/soap_ive_sm.png'
-import opener_down from '../../assets/ssc/opener_down.png'
+// import soap_opener from '../../assets/ssc/soap_opener_sm.png'
+// import soap_ive from '../../assets/ssc/soap_ive_sm.png'
+// import opener_down from '../../assets/ssc/opener_down.png'
+import ssc_triple from '../../assets/ssc/ssc_triple.png'
 
 export const OpenerMobile = () => {
   return (
     <section className='OpenerContainer'>
       <div className='Opener'>
-        <img className='OpenerFoto' src={soap_opener} alt={soap_opener} />
-        <img className='OpenerFoto' src={opener_down} alt={opener_down} />
-        <img className='OpenerFoto' src={soap_ive} alt={soap_ive} />
+        <img className='OpenerFoto' src={ssc_triple} alt={ssc_triple} />
+        {/* <img className='OpenerFoto' src={opener_down} alt={opener_down} />
+        <img className='OpenerFoto' src={soap_ive} alt={soap_ive} /> */}
       </div>
       <div className='textWrapper'>
         <h2 className='cases_openerText mobile'> Sustainable Soap Co.</h2>
