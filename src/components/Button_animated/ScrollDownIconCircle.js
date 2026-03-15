3d20446bda3baa56c0929ac9a8d221222cc75c88
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2026-03-15 16:57:25 -0400

    update arrow down on home page hover

--- src/components/Button_animated/ScrollDownIconCircle.js
+++ src/components/Button_animated/ScrollDownIconCircle.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { FaArrowDown } from 'react-icons/fa'
 import './ScrollDownIconCircle.css'
 
 const ScrollDownIconCircle = () => {
+  const arrowRef = useRef(null)
+
   const scrollToNextSection = () => {
     window.scrollTo({
       top: window.innerHeight,
@@ -10,9 +12,31 @@ const ScrollDownIconCircle = () => {
     })
   }
 
+  const playHoverAnimation = () => {
+    const arrow = arrowRef.current
+    if (!arrow) return
+
+    arrow.style.animation = 'none'
+    arrow.offsetHeight
+    arrow.style.animation = 'fade_move_down .8s ease-in-out 1'
+  }
+
+  useEffect(() => {
+    const arrow = arrowRef.current
+    if (!arrow) return
+
+    arrow.style.animation = 'fade_move_down 1.2s ease-in-out 3'
+  }, [])
+
   return (
-    <div className='scroll-down-circle' onClick={scrollToNextSection}>
-      <FaArrowDown />
+    <div
+      className='scroll-down-circle'
+      onClick={scrollToNextSection}
+      onMouseEnter={playHoverAnimation}
+    >
+      <span ref={arrowRef} className='scroll-down-arrow'>
+        <FaArrowDown />
+      </span>
     </div>
   )
 }
